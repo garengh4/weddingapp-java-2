4dfc315b669de5606ae0700fbf74e397b8a0cc34
unknown <garengh4@gmail.com>
2022-04-17 16:10:03 -0400

    Removed keys from application.properties

--- backyardbackend/src/main/resources/tablescript.sql
+++ backyardbackend/src/main/resources/tablescript.sql
@@ -41,7 +41,7 @@ CREATE TABLE event(
 );
 
 
---@block
+
 INSERT INTO customer (first_name, last_name) VALUES 
     ("Kyla", "Friso"),
     ("Hadi", "Robertson");
@@ -49,17 +49,17 @@ INSERT INTO customer (first_name, last_name) VALUES
 INSERT INTO partner(first_name, last_name, partner_rating) VALUES 
     ("Julia", "Bubbles", 3.5),
     ("Ann", "Tseng", 4.5);
---@block
+
 INSERT INTO backyard(backyard_description, backyard_rating, backyard_city, backyard_cost, partner_id) VALUES
     ("beautiful hillside", 4.0, "Toronto", "2000", 001),    -- Julia's backyard
     ("cabin next to the lake", 3.0, "Kelowna", 1500, 002),  -- Ann's backyard
     ("the cafe", 2.5, "Montreal", 1000, 001);               -- Julia's backyard
---@block
+
 INSERT INTO event (event_name, event_date, customer_id, backyard_id) VALUES 
     ("getting married", "2022-08-23", 001, 001),            -- customer Kyla at Julia's backyard in Toronto
     ("wedding anniversary", "2023-08-23", 001, 001),        -- customer kyla at Julia's backyard in Toronto
     ("getting married", "2023-01-15", 002, 002),            -- customer Hadi at Ann's backyard in Kelowna
     ("gender reveal", "2025-03-15", 001, 002),              -- customer Kyla at Ann's backyard in Toronto
     ("gender reveal", "2024-04-23", 002, 003);              -- customer Hadi at Julia's backyard in Montreal
---@block
+
 
