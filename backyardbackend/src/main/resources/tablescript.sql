b7f85dfad90296df79139981258344b190d2e3fd
unknown <garengh4@gmail.com>
2022-04-24 17:43:24 -0400

    added password to sql and change a few things

--- backyardbackend/src/main/resources/tablescript.sql
+++ backyardbackend/src/main/resources/tablescript.sql
@@ -7,6 +7,8 @@ CREATE TABLE customer (
   customer_id INT AUTO_INCREMENT,
   first_name VARCHAR(25),
   last_name VARCHAR(25),
+  password VARCHAR(70) NOT NULL,
+  email_id VARCHAR(50),
   CONSTRAINT pk_customer PRIMARY KEY (customer_id)
 );
 
@@ -14,14 +16,15 @@ CREATE TABLE partner (
   partner_id INT AUTO_INCREMENT,
   first_name VARCHAR(25),
   last_name VARCHAR(25),
-  partner_rating INT,
+  password VARCHAR(70) NOT NULL,
+  email_id VARCHAR(50),
   CONSTRAINT pk_partner PRIMARY KEY (partner_id)
 );
 
 CREATE TABLE backyard (
   backyard_id INT AUTO_INCREMENT,
+  backyard_name VARCHAR(50),
   backyard_description VARCHAR(150),
-  backyard_rating INT,
   backyard_city VARCHAR(25),
   backyard_cost INT,
   partner_id INT,
@@ -32,6 +35,7 @@ CREATE TABLE backyard (
 CREATE TABLE event(
   event_id INT AUTO_INCREMENT,
   event_name VARCHAR(50),
+  event_description VARCHAR(150),
   event_date DATE,
   customer_id INT,
   backyard_id INT,
@@ -41,25 +45,25 @@ CREATE TABLE event(
 );
 
 
-
-INSERT INTO customer (first_name, last_name) VALUES 
-    ("Kyla", "Friso"),
-    ("Hadi", "Robertson");
+INSERT INTO customer (first_name, last_name, password, email_id) VALUES 
+    ("Kyla", "Friso", "KylaFriso@", "kylafriso@gmail.com"),
+    ("Hadi", "Robertson","HadiR%", "hadirobertson@gmail.com");
     
-INSERT INTO partner(first_name, last_name, partner_rating) VALUES 
-    ("Julia", "Bubbles", 3.5),
-    ("Ann", "Tseng", 4.5);
+INSERT INTO partner(first_name, last_name, password, email_id) VALUES 
+    ("Julia", "Bubbles", "Juliabb@", "juliabubbles@gmail.com"),
+    ("Ann", "Tseng", "annT$", "anntseng@gmail.com");
 
-INSERT INTO backyard(backyard_description, backyard_rating, backyard_city, backyard_cost, partner_id) VALUES
-    ("beautiful hillside", 4.0, "Toronto", "2000", 001),    -- Julia's backyard
-    ("cabin next to the lake", 3.0, "Kelowna", 1500, 002),  -- Ann's backyard
-    ("the cafe", 2.5, "Montreal", 1000, 001);               -- Julia's backyard
+--@block
+INSERT INTO backyard(backyard_name, backyard_description, backyard_city, backyard_cost, partner_id) VALUES
+    ("Binxy J", "beautiful hillside", "Toronto", 2300, 001),        -- Julia's backyard
+    ("Kitty C", "cat cafe", "Montreal", 3400, 001),                 -- Julia's backyard
+    ("Ribbit A", "cabin next to the lake", "Kelowna", 1700, 002);   -- Ann's backyard
 
-INSERT INTO event (event_name, event_date, customer_id, backyard_id) VALUES 
-    ("getting married", "2022-08-23", 001, 001),            -- customer Kyla at Julia's backyard in Toronto
-    ("wedding anniversary", "2023-08-23", 001, 001),        -- customer kyla at Julia's backyard in Toronto
-    ("getting married", "2023-01-15", 002, 002),            -- customer Hadi at Ann's backyard in Kelowna
-    ("gender reveal", "2025-03-15", 001, 002),              -- customer Kyla at Ann's backyard in Toronto
-    ("gender reveal", "2024-04-23", 002, 003);              -- customer Hadi at Julia's backyard in Montreal
+INSERT INTO event (event_name, event_description, event_date, customer_id, backyard_id) VALUES 
+    ("K&A", "getting married","2022-08-23", 001, 001),                    -- customer Kyla at Julia's backyard in Toronto
+    ("K&A2", "wedding anniversary", "2023-08-23", 001, 001),              -- customer kyla at Julia's backyard in Toronto
+    ("Hadi loves Robbie", "getting married", "2022-01-14", 002, 003),     -- customer Hadi at Ann's backyard in Kelowna
+    ("K junior", "gender reveal", "2025-03-15", 001, 003),                -- customer Kyla at Ann's backyard in Kelowna
+    ("H&R junior", "gender reveal", "2024-04-23",002, 002);               -- customer Hadi at Julia's backyard in Montreal
 
 
